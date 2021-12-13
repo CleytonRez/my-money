4e5f163ce31bbfbb1c30b35539c1263861cf1c41
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-12-13 18:25:33 -0300

    feature: Create MyMoney Project, Using Firebase Realtime Database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useGet from "./useGet";
 
+/*
+axios
+.get('https://mymoney-cleyton-default-rtdb.firebaseio.com/valor.json')
+.then(res => {
+  console.log(res.data)
+})*/
+
+/*
+axios
+.post('https://mymoney-cleyton-default-rtdb.firebaseio.com/valor.json', {
+  outro: 'Cleyton Rezende'
+})
+.then(res=> {
+  console.log(res)
+})
+*/
+
+// Variavel contendo a url.
+const url = 'https://mymoney-cleyton-default-rtdb.firebaseio.com/movimentacoes/2019-08.json'
+
+// Componente Pricipal
 function App() {
+
+  //Variavel que chama a funcao useGet.
+  const data = useGet(url)
+
+  //Variavel contendo a url 2.
+  const data2 = useGet('http://httpbin.org/ip')
+
+  // Retorna o HTML Principal.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MyMoney</h1>
+      {JSON.stringify(data)}
+      {data.loading && <p>Loading...</p>}
+      <pre>{JSON.stringify(data2)}</pre>
     </div>
   );
 }
